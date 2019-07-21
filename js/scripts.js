4e732f04446a2e2ac8c38ceec1023b3d8be53a15
jessmcdonald <jessica.mcdonald@zalando.de>
2019-07-22 00:54:50 +0200

    replace for loop with foreach
    commented out forloop
    replaced with foreach
    to print pokemon details

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,21 @@ const pokemons = [
   {name: 'Jigglypuff', height: 50, types: ['fairy', ' normal'], img: 'img/jigglypuff.png'}
 ];
 
+
+/*for loop to print pokemon details
+for (var i = 0; i < pokemons.length; i++){
+  if(pokemons[i].height > 65){
+    document.write('<div class="pokemon"><img src=' + pokemons[i].img + ' width=200><br><h3>' + pokemons[i].name + '</h4><br><b>height: </b>' + pokemons[i].height + ' (woah that\'s big!)<br><b>types: </b>' + pokemons[i].types + '</div><p>')
+  }else{
+    document.write('<div class="pokemon"><img src=' + pokemons[i].img + ' width=200><br><h3>' + pokemons[i].name + '</h4><br><b>height: </b>' + pokemons[i].height + '<br><b>types: </b>' + pokemons[i].types + '</div><p>')
+  }
+}
+*/
+
+//foreach to print pokemon details
 pokemons.forEach(pokemon =>
   (pokemon.height > 65) ?
-    (document.write('<div class="pokemon"><img src="' + pokemon.img + '" width=200><br><h3>' + pokemon.name + '</h4><br><b>height:</b> ' + pokemon.height + '   (woah that\'s big!)<br><b>types:</b> ' + pokemon.types + '</div><p>'))
+    (document.write('<div class="pokemon"><img src="' + pokemon.img + '" width=200><br><h3>' + pokemon.name + '</h4><br><b>height:</b> ' + pokemon.height + ' (woah that\'s big!)<br><b>types:</b> ' + pokemon.types + '</div><p>'))
     :
     (document.write('<div class="pokemon"><img src="' + pokemon.img + '" width=200><br><h3>' + pokemon.name + '</h4><br><b>height:</b> ' + pokemon.height + '<br><b>types:</b> ' + pokemon.types + '</div><p>'))
   );
-
-
-/* WIP
-  function changeTypesFontColor() {
-        if (pokemon.types = 'fire' {
-        text.color = '#99C262';
-        } else
-
-*/
