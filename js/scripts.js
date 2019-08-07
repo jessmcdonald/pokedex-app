17c30ac26634f91d0217efe42601dc85c28ad563
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2019-08-07 19:31:39 +0200

    added search bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 
 var pokemonRepository = (function () {
+  const searchInput = document.querySelector('.search-bar__input');
   var pokemons = [];
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
@@ -191,6 +192,29 @@ pokemonRepository
 });
 }
 
+//search bar function
+function searchFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("pokeSearchInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("pokemon-list");
+  li = ul.getElementsByTagName("li");
+
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("button")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+
 window.addEventListener("keydown", (e) => {
   var $pokemonModalContainer = document.querySelector("#modal-container");
   if (e.key === 'Escape' && $pokemonModalContainer.classList.contains("is-visible")){
@@ -206,6 +230,8 @@ document.querySelector('#modal-container').addEventListener("click", (e) => {
   }
 });
 
+
+
   //public functions
   return {
     add: add,
@@ -217,7 +243,8 @@ document.querySelector('#modal-container').addEventListener("click", (e) => {
     loadList: loadList,
     loadDetails: loadDetails,
     showPokemonModal: showPokemonModal,
-    hidePokemonModal: hidePokemonModal
+    hidePokemonModal: hidePokemonModal,
+    searchFunction: searchFunction
   };
 })();
 
